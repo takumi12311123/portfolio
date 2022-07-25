40ae6a99575b9bc832bde17d96be30ca936d3f8b
Takuaka1231 <takumiakasaka1231@gmail.com>
2022-07-25 21:35:44 +0900

    allskillを編集

--- src/component/AllSkill.jsx
+++ src/component/AllSkill.jsx
@@ -3,6 +3,8 @@ import reactImage from "../Images/react.png";
 import jsImage from "../Images/js.png";
 import graphqlImage from "../Images/graphql.png";
 import typescriptImage from "../Images/TypeScript.png";
+import pythonImage from "../Images/python.png";
+import fastAPIImage from "../Images/fastapi-1.svg";
 
 export const AllSkill = () => {
   return (
@@ -12,13 +14,18 @@ export const AllSkill = () => {
         <div class="row text-center">
           <div class="col-md-4 services">
             <img src={typescriptImage} />
-            <h4>TypeScript/JavaScript/Python</h4>
-            <p>TypeScript/JavaScript/Pythonがつかえます</p>
+            <h4>TypeScript</h4>
+            <p>TypeScriptがつかえます</p>
           </div>
           <div class="col-md-4 services">
             <img src={jsImage} />
             <h4>JavaScript</h4>
             <p>JavaScriptがつかえます</p>
+          </div>
+          <div class="col-md-4 services">
+            <img src={pythonImage} />
+            <h4>Python</h4>
+            <p>Pythonがつかえます</p>
           </div>
           <div class="col-md-4 services">
             <img src={reactImage} />
@@ -29,6 +36,11 @@ export const AllSkill = () => {
             <img src={graphqlImage} />
             <h4>GraphQL</h4>
             <p>GraphQLがつかえます</p>
+          </div>
+          <div class="col-md-4 services">
+            <img src={fastAPIImage} />
+            <h4>fastAPI</h4>
+            <p>fastAPIがつかえます</p>
           </div>
         </div>
       </div>
--- src/component/HomePage.jsx
+++ src/component/HomePage.jsx
@@ -5,19 +5,20 @@ import jsImage from "../Images/js.png";
 import firebaseImage from "../Images/firebase.jpg";
 import profileImage from "../Images/sibainu.jpg";
 import typescriptImage from "../Images/TypeScript.png";
+import graphqlImage from "../Images/graphql.png";
+import { Link } from "react-router-dom";
 
 export const HomePage = () => {
   return (
     <div className="container text-center">
       <h1>赤坂匠のポートフォリオ</h1>
-
       <img src={profileImage} className="profileImage" />
-
+      <p>青山学院大学陸上競技部長距離ブロック、駅伝部に所属しています。</p>
       <p>
-        青山学院大学陸上競技部長距離ブロック、駅伝部に所属しています。
         大学生になって、陸上を怪我で引退し、マネージャーとして部活動を続けながら、
-        プログラミングの勉強を日々しています。 得意言語は、TypeScriptです
+        プログラミングの勉強を日々しています。
       </p>
+      <p>得意言語は、TypeScriptです</p>
 
       <section class="page-section" id="services">
         <div className="service">
@@ -27,47 +28,22 @@ export const HomePage = () => {
               私が作った作品一覧です
             </h3>
           </div>
-          <div class="row text-center">
-            <div class="col-md-4">
-              <span class="fa-stack fa-4x">
-                <i class="fas fa-circle fa-stack-2x text-primary"></i>
-                <i class="fas fa-shopping-cart fa-stack-1x fa-inverse"></i>
-              </span>
-              <h4 class="my-3">ECサイト</h4>
-              <p class="text-muted">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima
-                maxime quam architecto quo inventore harum ex magni, dicta
-                impedit.
-              </p>
-            </div>
+          <div class="row text-center" textAlign="center">
             <div class="col-md-4">
               <span class="fa-stack fa-4x">
                 <i class="fas fa-circle fa-stack-2x text-primary"></i>
                 <i class="fas fa-laptop fa-stack-1x fa-inverse"></i>
               </span>
-              <h4 class="my-3">レスポンシブサイト</h4>
+              <h4 class="my-3">webアプリケーション</h4>
               <p class="text-muted">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima
-                maxime quam architecto quo inventore harum ex magni, dicta
-                impedit.
-              </p>
-            </div>
-            <div class="col-md-4">
-              <span class="fa-stack fa-4x">
-                <i class="fas fa-circle fa-stack-2x text-primary"></i>
-                <i class="fas fa-lock fa-stack-1x fa-inverse"></i>
-              </span>
-              <h4 class="my-3">ウェブセキュリティ</h4>
-              <p class="text-muted">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima
-                maxime quam architecto quo inventore harum ex magni, dicta
-                impedit.
+                バックエンドとフロントエンドを両方できます。
+                フルスタックエンジニアを目標にしています。
+                ReactとgraphQLを使うことが多いです。
               </p>
             </div>
           </div>
         </div>
       </section>
-
       <section id="skill">
         <div class="text-center">
           <h1 class="title">スキル</h1>
@@ -83,14 +59,16 @@ export const HomePage = () => {
               <p>Reactがつかえます</p>
             </div>
             <div class="col-md-4 services">
-              <img src={jsImage} />
+              <img src={graphqlImage} />
               <h4>GraphQL/fastAPI/Django</h4>
               <p>GraphQL/fastAPI/Djangoがつかえます</p>
             </div>
           </div>
-          <button src="/skill" type="button" class="btn btn-primary">
-            スキル一覧
-          </button>
+          <Link to={`/skills`}>
+            <button type="button" class="btn btn-primary">
+              スキル一覧
+            </button>
+          </Link>
         </div>
       </section>
     </div>
