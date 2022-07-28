70422f28452e0c6e82db7593b4184b27b3e7b379
Takuaka1231 <takumiakasaka1231@gmail.com>
2022-07-28 21:47:15 +0900

    内容の変更

--- src/component/MyPage.jsx
+++ src/component/MyPage.jsx
@@ -11,6 +11,14 @@ export const MyPage = () => {
         className="profileImage"
       />
       <h1>経歴</h1>
+      <h3>2001.12.31誕生</h3>
+      <h3>2007年けやき小学校入学</h3>
+      <h3>2013年明照学園樹徳中学校入学</h3>
+      <h3>2016年明照学園樹徳高校入学</h3>
+      <h3>2019年青山学院大学入学</h3>
+      <p>大学生になり、プログラミングと出会う</p>
+      <p>HTML,CSS,JavaScript,TypeScript,Pythonといった言語を扱う</p>
+      <p>フレームワークは、React,GraphQL,Django,fastAPIを扱う</p>
     </div>
   );
 };
